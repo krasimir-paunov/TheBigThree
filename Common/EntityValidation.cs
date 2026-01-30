b651886fc108adf44df15c4db0ff6e84f58edd7c
krasimir-paunov <krasimirsb86re@gmail.com>
2026-01-30 11:15:50 +0200

    Add Game Model

--- Common/EntityValidation.cs
+++ Common/EntityValidation.cs
@@ -2,6 +2,15 @@
 {
     public static class EntityValidation
     {
+        // Genre
+        public const int GenreNameMinLength = 3;
+        public const int GenreNameMaxLength = 50;
 
+        // Game
+        public const int GameTitleMinLength = 1;
+        public const int GameTitleMaxLength = 150;
+        public const int GameImageUrlMaxLength = 2048;
+        public const int GameDescriptionMinLength = 20;
+        public const int GameDescriptionMaxLength = 2000;
     }
 }
--- Models/Genre.cs
+++ Models/Genre.cs
@@ -1,6 +1,17 @@
-﻿namespace TheBigThree.Models
+﻿using System.ComponentModel.DataAnnotations;
+using static TheBigThree.Common.EntityValidation;
+
+namespace TheBigThree.Models
 {
     public class Genre
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GenreNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Game> Games { get; set; } = new HashSet<Game>();
     }
 }
