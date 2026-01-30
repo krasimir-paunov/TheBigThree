6e106b7eb06c84faad283e9c2970b20ab4898242
krasimir-paunov <krasimirsb86re@gmail.com>
2026-01-30 13:30:36 +0200

    Add IActionResult Add [HttpPost] to CollectionController

--- Controllers/CollectionController.cs
+++ Controllers/CollectionController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using TheBigThree.Contracts;
+using TheBigThree.ViewModels;
 
 namespace TheBigThree.Controllers
 {
+    [Authorize]
     public class CollectionController : Controller
     {
         private readonly ICollectionService collectionService;
@@ -26,6 +29,32 @@ namespace TheBigThree.Controllers
             var model = await collectionService.GetMineCollectionsAsync(userId);
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = await collectionService.GetNewAddFormModelAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CollectionFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var refreshModel = await collectionService.GetNewAddFormModelAsync();
+                model.Games.ForEach(g => g.Genres = refreshModel.Games[0].Genres);
+
+                return View(model);
+            }
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            await collectionService.AddCollectionAsync(model, userId);
+
+            return RedirectToAction(nameof(All));
         }
     }
 }
