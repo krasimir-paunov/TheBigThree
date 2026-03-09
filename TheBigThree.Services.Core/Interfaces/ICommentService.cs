af560453ee0d3be288f35e8119833cb29df66c92
krasimir-paunov <krasimirsb86re@gmail.com>
2026-03-09 16:53:21 +0200

    Add CommentViewModel to Web/ViewModels

---
+++ TheBigThree.Services.Core/Interfaces/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using TheBigThree.Data.Models;
+
+namespace TheBigThree.Services.Core.Interfaces
+{
+    public interface ICommentService
+    {
+        Task<Comment> GetCommentsByCollectionIdAsync(int collectionId);
+
+        Task AddCommentAsync(string content, int collectionId, string userId);
+
+        Task DeleteCommentAsync(int commentId, string userId);
+
+        Task AdminDeleteCommentAsync(int commentId);
+
+    }
+}
---
+++ TheBigThree.Web.ViewModels/CommentViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TheBigThree.Web.ViewModels
+{
+    public class CommentViewModel
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        public string UserName { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+    }
+}
