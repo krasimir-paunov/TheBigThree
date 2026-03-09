8b881a14bcf27f5756643c750a9bfe1239758ddf
krasimir-paunov <krasimirsb86re@gmail.com>
2026-03-09 17:36:30 +0200

    Add generic IRepository<T> interface

---
+++ TheBigThree.Services.Core/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace TheBigThree.Services.Core.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task SaveChangesAsync();
+
+
+    }
+}
