69d8ec67507c51983cfc34c901cbc228953c0bfb
krasimir-paunov <krasimirsb86re@gmail.com>
2026-03-16 10:25:18 +0200

    Add account management pages

---
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,81 @@
+﻿#nullable disable
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheBigThree.Data.Models;
+
+namespace TheBigThree.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        [BindProperty]
+        public string AvatarUrl { get; set; }
+        public string Rank { get; set; }
+        public DateTime MemberSince { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            Username = user.UserName;
+            Email = user.Email;
+            AvatarUrl = user.AvatarUrl;
+            MemberSince = user.CreatedOn;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+
+                return Page();
+            }
+
+            user.AvatarUrl = AvatarUrl;
+
+            await _userManager.UpdateAsync(user);
+
+            await _signInManager.RefreshSignInAsync(user);
+
+            StatusMessage = "Your profile has been updated";
+
+            return RedirectToPage();
+        }
+    }
+}
